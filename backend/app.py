dcc5ef9e621831bfa7d6bd7c1da6e9219916adde
Ryan <azbo400@gmail.com>
2024-01-20 17:19:24 -0800

    Added web socket

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,44 @@
+from arduino_reader import retrieve_from_arduino
 from flask import Flask
+from flask import request
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS, cross_origin
+
+# This is the main API/socket.io interface 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
-def test():
-    return {
-        "test": "Hello, World!"
-    }
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+loop = True
+
+# Socket.io connection logic
+@socketio.on('connect')
+def handle_connect():
+    global loop
+    loop = True
+    print('connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global loop
+    print('disconnected')
+    loop = False
+
+@socketio.on('message')
+def handle_message(message):
+    print(f'Received message: {message}')
+    emit('message', message)
+
+@socketio.on('get_data')
+def send_data(test):
+    while loop:  # Replace this with a condition to stop sending data
+        data = retrieve_from_arduino()
+        print(data)
+        socketio.emit('data_response', {'data': data})
+        socketio.sleep(1)  # Adjust the interval as needed
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    socketio.run(app, host='0.0.0.0', port=105)
--- backend/arduino_reader.py
+++ backend/arduino_reader.py
@@ -3,15 +3,19 @@ import time
 from dotenv import load_dotenv
 import os
 
+# Pulls data from the Serial logs of the Arduino (code of which is written in an Arduino IDE)
+
 load_dotenv()  # take environment variables from .env.
 
 ser = serial.Serial(os.environ.get('ARDUINO_PORT'), 9600, timeout=0.1)
 
-def write_read(): 
+def retrieve_from_arduino(): 
     time.sleep(0.05)
     data = ser.readline()
     return data
 
-while True:
-    value = write_read()
-    print(value)
+# this code is commented out because the web socket handles looping and calling `write_read` now. To prototype uncomment
+# while True:
+#     value = retrieve_from_arduino()
+#     print(value)
+
