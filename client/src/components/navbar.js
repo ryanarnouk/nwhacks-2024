b4bc908cc95ec9bef83913c7e23b1d5ee339ba8e
Ryan <azbo400@gmail.com>
2024-01-20 23:41:16 -0800

    Update app name and add logo

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='w-full flex flex-row justify-between p-3'>
-        <h1 className='font-bold text-5xl dark:text-dark-tremor-content-emphasis'>App Title</h1>
+        <h1 className='font-bold text-5xl dark:text-dark-tremor-content-emphasis'>Breathe</h1>
         <div className='flex flex-row justify-around'>
             <a className='dark:text-dark-tremor-content-emphasis hover:text-sky-300 font-bold py-2 px-4 rounded'>
                 Respiration
